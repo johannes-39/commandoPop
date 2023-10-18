ec04031eacf92bc26bbedc08900bbedd7901eda9
JST <johannes.staedtler@iso-gruppe.com>
2023-10-18 15:11:43 +0200

    buy button added

--- src/components/ButtonComponent/ButtonComponent.js
+++ src/components/ButtonComponent/ButtonComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './styles.css';
 const ButtonComponent:React.FC<any> =  () => {
     return (
-        <button className={'button'}>test</button>
+        <button className={'button'}>kaufen</button>
     )
 }
 export default ButtonComponent;
--- src/components/TextComponent/TextComponent.js
+++ src/components/TextComponent/TextComponent.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './styles.css'
+import ButtonComponent from "../ButtonComponent/ButtonComponent";
 const TextComponent:React.FC<any> =  ({myclick, input}) => {
     console.log(myclick);
     return (
         <div>
             {
-                myclick === false ? <p  className={'p'}>{input}</p>: <div><p  className={'p1'}>{input}<div className={'line'}></div></p></div>}
+                myclick === false ? <p  className={'p'}>{input}</p>: <div><p  className={'p1'}>{input}<div className={'line'}></div>
+                <div className={"purchase"}>
+                    <p  className={'p2'}>{"commando poppenreuth shirt"}</p>
+                    <ButtonComponent></ButtonComponent>
+
+
+                </div>
+                </p></div>}
         </div>
     )
 }
