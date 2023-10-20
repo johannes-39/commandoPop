5cf28c358a6226fdbc3e9e411d73cda23fdbc040
JST <johannes.staedtler@iso-gruppe.com>
2023-10-20 20:24:31 +0200

    addad news

--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -8,6 +8,10 @@ function News() {
     return (
         <div className={"divCompNews"}>
             <GameComponent team1logo={image1} team2logo={image1} team1score={"1"} team2score={"2"} info={"sonntag 15:00"}></GameComponent>
+            <GameComponent team1logo={image1} team2logo={image1} team1score={"1"} team2score={"2"} info={"sonntag 15:00"}></GameComponent><GameComponent team1logo={image1} team2logo={image1} team1score={"1"} team2score={"2"} info={"sonntag 15:00"}></GameComponent>
+            <GameComponent team1logo={image1} team2logo={image1} team1score={"1"} team2score={"2"} info={"sonntag 15:00"}></GameComponent>
+            <GameComponent team1logo={image1} team2logo={image1} team1score={"1"} team2score={"2"} info={"sonntag 15:00"}></GameComponent>
+
             <ArtikelComponent image={image} content={"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."}></ArtikelComponent>
         </div>
 
