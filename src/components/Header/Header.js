9e0b6e068efeafd3b852986e637a9c65529e6c4c
JST <johannes.staedtler@iso-gruppe.com>
2023-10-23 17:33:04 +0200

    fixing 2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './styles.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import News from "../../pages/news/News";
+import Home from "../../pages/home/Home";
+import About from "../../pages/about/About";
 const Header:React.FC<any> =  () => {
     return (
         <div className={'div'}>
