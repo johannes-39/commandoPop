dc8052b4d006cd2d7a4cd467ae54430834f2ad16
JST <johannes.staedtler@iso-gruppe.com>
2023-10-23 17:19:27 +0200

    addad news artikel real text

--- src/pages/news/News.js
+++ src/pages/news/News.js
@@ -21,7 +21,7 @@ function News() {
             </div>
             <GameComponent team1logo={image3} team2logo={image1} team1score={"1"} team2score={"2"} info={"sonntag 15:00"}></GameComponent>
 
-            <ArtikelComponent image={image} content={"Der SV Fürth-Poppenreuth gewann sein Heimspiel gegen die SG Puschendorf/Tuchenbach ungefährdet mit 4:1 und behauptete damit den dritten Tabellenplatz. Nach der frühen 2:0-Führung durch Philipp Müller und Dominik Döll verkürzte Paul Bernreuther zwar, doch erneut Döll und Stefano Deviggiano stellten in der zweiten Halbzeit den Endstand her. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet."}></ArtikelComponent>
+            <ArtikelComponent image={image} content={"Der SV Fürth-Poppenreuth gewann sein Heimspiel gegen die SG Puschendorf/Tuchenbach ungefährdet mit 4:1 und behauptete damit den dritten Tabellenplatz. Nach der frühen 2:0-Führung durch Philipp Müller und Dominik Döll verkürzte Paul Bernreuther zwar, doch erneut Döll und Stefano Deviggiano stellten in der zweiten Halbzeit den Endstand her."}></ArtikelComponent>
         </div>
 
 
